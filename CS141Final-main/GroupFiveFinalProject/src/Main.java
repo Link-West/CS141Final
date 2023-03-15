94ee2cd5242bacd262c64a1ae6dccd8587cfd8d0
Link-West <127905912+Link-West@users.noreply.github.com>
2023-03-15 08:58:11 -0700

    Add files via upload
    Added expectations in commentary in Main

---
+++ CS141Final-main/GroupFiveFinalProject/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Basic: Create a program that does conversions between Standard (metric)
+        and Imperial (US/UK) units of measurement.
+
+        Extended: Create classes for Distance/Length (Link), Weight/Mass (Zion),
+        Temperature (Link), and Volume (Fernando) units of measurement.
+        Classes will have the fields of:
+        double value
+        String unit_of_measurement
+
+        Classes will have constructors so people can create and alter data
+        objects.
+
+        Classes will have non-static methods so the objects can output their
+        value in other units of measurement
+
+        Classes will have static methods so the Class can conduct conversions,
+        when first given a value.
+
+        */
+
+/*      Testing temperature conversions
+        Temperature aZero = new Temperature(0,"K");
+        Temperature sBoil = new Temperature(100, "C");
+        Temperature iFreeze = new Temperature(32, "F");
+        System.out.println("Non-static");
+        System.out.println(aZero.toCelsius());
+        System.out.println(aZero.toFahrenheit());
+        System.out.println(sBoil.toKelvin());
+        System.out.println(sBoil.toFahrenheit());
+        System.out.println(iFreeze.toCelsius());
+        System.out.println(iFreeze.toKelvin());
+        System.out.println("Static");
+        System.out.println(Temperature.KtoC(0));
+        System.out.println(Temperature.KtoF(373));
+        System.out.println(Temperature.CtoF(0));
+        System.out.println(Temperature.CtoK(0));
+        System.out.println(Temperature.FtoC(32));
+        System.out.println(Temperature.FtoK(212));*/
+
+    }
+}
---
+++ CS141Final-main/GroupFiveFinalProject/src/Volume.java
@@ -0,0 +1,6 @@
+public class Volume { //Fernando
+    public int value;
+    public String unit_of_measurement;
+//    public String abbreviated_units;
+
+}
