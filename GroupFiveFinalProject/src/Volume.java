26ca9b9e274f870b60ad61ddd2c827ebda46c0af
Link-West <127905912+Link-West@users.noreply.github.com>
2023-03-15 09:00:08 -0700

    Delete GroupFiveFinalProject directory
    removed file w/o to-do comments

--- GroupFiveFinalProject/src/Volume.java
+++
@@ -1,6 +0,0 @@
-public class Volume {
-    public int value;
-    public String unit_of_measurement;
-//    public String abbreviated_units;
-
-}
--- GroupFiveFinalProject/src/Weight.java
+++
@@ -1,6 +0,0 @@
-public class Weight {
-    public int value;
-    public String unit_of_measurement;
-//    public String abbreviated_units;
-
-}
