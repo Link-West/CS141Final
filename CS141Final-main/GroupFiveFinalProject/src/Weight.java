e7854ab606140613675afcca272fa98212a8667f
zjasso123 <103480920+zjasso123@users.noreply.github.com>
2023-03-26 04:28:59 -0700

    Update Weight.java

--- CS141Final-main/GroupFiveFinalProject/src/Weight.java
+++ CS141Final-main/GroupFiveFinalProject/src/Weight.java
@@ -1,54 +1,105 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
 
-public class WeightConverter { //Zion
-    private double pounds; // stores the weight in pounds
-    
-    // Constructor that takes a weight in pounds as input
+public class WeightConverter {
+    private double pounds;
+    private double kilograms;
+    private double grams;
+    private double ounces;
+    private double tons;
+    private double milligrams;
+    private double micrograms;
+    private double nanograms;
+
+    // Constructor that takes in the weight in pounds
     public WeightConverter(double pounds) {
         this.pounds = pounds;
+        convertToKilograms();
+        convertToGrams();
+        convertToOunces();
+        convertToTons();
+        convertToMilligrams();
+        convertToMicrograms();
+        convertToNanograms();
+    }
+
+    // Method to convert pounds to kilograms
+    private void convertToKilograms() {
+        kilograms = pounds * 0.453592;
+    }
+
+    // Method to convert pounds to grams
+    private void convertToGrams() {
+        grams = pounds * 453.592;
+    }
+
+    // Method to convert pounds to ounces
+    private void convertToOunces() {
+        ounces = pounds * 16;
     }
     
-    // Method that converts pounds to kilograms
-    public double toKilograms() {
-        return pounds * 0.453592;
+    // Method to convert pounds to tons
+    private void convertToTons() {
+        tons = pounds / 2000;
     }
     
-    // Method that converts pounds to grams
-    public double toGrams() {
-        return pounds * 453.592;
+    // Method to convert pounds to milligrams
+    private void convertToMilligrams() {
+        milligrams = pounds * 453592.37;
     }
     
-    // Method that converts pounds to ounces
-    public double toOunces() {
-        return pounds * 16.0;
+    // Method to convert pounds to micrograms
+    private void convertToMicrograms() {
+        micrograms = pounds * 4.5359237E+8;
     }
     
-    // toString method that returns a string representation of the weight in all three mass types
-    public String toString() {
-        return String.format("%.2f pounds = %.2f kilograms = %.2f grams = %.2f ounces", 
-                pounds, toKilograms(), toGrams(), toOunces());
+    // Method to convert pounds to nanograms
+    private void convertToNanograms() {
+        nanograms = pounds * 4.5359237E+11;
     }
-    
-    public static void main(String[] args) {
-        // Create a new WeightConverter object with a random weight between 1 and 100 pounds
+
+    // Method to generate a random weight in pounds
+    public static double generateRandomWeight() {
         Random rand = new Random();
-        double weight = rand.nextDouble() * 99.0 + 1.0;
-        WeightConverter wc = new WeightConverter(weight);
-        System.out.println("Random weight: " + wc.pounds + " pounds");
-        
-        // Read a file containing a list of weights in pounds, and convert each weight to all three mass types
-        try (BufferedReader br = new BufferedReader(new FileReader("weights.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                double pounds = Double.parseDouble(line);
-                WeightConverter wc2 = new WeightConverter(pounds);
-                System.out.println(wc2.toString());
+        double weight = rand.nextDouble() * 100;
+        return weight;
+    }
+
+    // Method to read weights from a file and convert them
+    public static void convertWeightsFromFile(String filename) {
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                double pounds = Double.parseDouble(scanner.nextLine());
+                WeightConverter converter = new WeightConverter(pounds);
+                System.out.println(converter.toString());
             }
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
         }
+    }
+
+    // toString method to display the weights in different units
+    public String toString() {
+        return "Pounds: " + pounds + "\nKilograms: " + kilograms + "\nGrams: " + grams + "\nOunces: " + ounces 
+            + "\nTons: " + tons + "\nMilligrams: " + milligrams + "\nMicrograms: " + micrograms + "\nNanograms: " + nanograms;
+    }
+
+    public static void main(String[] args) {
+        // Generate a random weight and convert it
+        double randomWeight = generateRandomWeight();
+        WeightConverter converter1 = new WeightConverter(randomWeight);
+        System.out.println("Random Weight Conversion:");
+        System.out.println(converter1.toString());
+
+        // Convert weights from a file
+        System.out.println("\nFile Input Conversion:");
+        convertWeightsFromFile("weights.txt");
     }
 }
